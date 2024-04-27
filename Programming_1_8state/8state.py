6526461a2a600cc48baaf5accf4cc89a31b7e3c3
Jacob Campbell <JacobCampbell2017@gmail.com>
2024-04-27 12:24:43 -0700

    Heuristic 1

--- Programming_1_8state/8state.py
+++ Programming_1_8state/8state.py
@@ -13,7 +13,7 @@ output: 3x3 grid where the tiles are in order
 
 Agent must determine if the puzzle is solvable, and if so, solve it.
 
-The agent will determine if the puzzle is solvable by counting the number of inversions in the puzzle. 
+The agent will determine if the puzzle is solvable by counting the number of inversions in the puzzle.
 If the number of inversions is even, the puzzle is solvable. If the number of inversions is odd, the puzzle is not solvable.
 
 The agent will solve the puzzle by using the A* algorithm with the hueristic that achieves the highest accuracy.
@@ -23,13 +23,14 @@ import random
 import time
 
 SOLVED_STATE1 = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+SOLVED_STATE2 = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
 
 ###############################
 ### Non-essential functions ###
 ###############################
 
 
-def randomize_state(state):
+def randomize_state(state) -> list[list[int]]:
     '''randomizes the state of the puzzle'''
     nums = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     random.shuffle(nums)
@@ -39,7 +40,7 @@ def randomize_state(state):
     return state
 
 
-def display_state(state):
+def display_state(state) -> None:
     print("-------")
     for i in range(3):
         print("|", end="")
@@ -52,7 +53,7 @@ def display_state(state):
 ###############################
 
 
-def is_solvable(state):
+def is_solvable(state) -> bool:
     '''determines if the puzzle is solvable'''
     inversions = 0
 
@@ -74,6 +75,24 @@ def is_solvable(state):
         return False
 
 
+def heuristic_1(state) -> tuple[int, list[list[int]]]:
+    '''Number of misplaced tiles heuristic'''
+    total1 = 0
+    total2 = 0
+    state_return = []
+    for i in range(3):
+        for j in range(3):
+            if state[i][j] != SOLVED_STATE1[i][j]:
+                total1 += 1
+            if state[i][j] != SOLVED_STATE2[i][j]:
+                total2 += 1
+    if total1 <= total2:
+        state_return = SOLVED_STATE1
+    else:
+        state_return = SOLVED_STATE2
+    return min(total1, total2), state_return
+
+
 def main():
     '''main function'''
     random.seed(time.time())
@@ -81,8 +100,8 @@ def main():
     state = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
     state = randomize_state(state)
 
-    is_solvable(state)
     display_state(state)
+    print(heuristic_1(state))
 
 
 if __name__ == "__main__":
